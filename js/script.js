435bbc788dca0050568bc8617812a88bbbf535a8
andreacassini <andrea1316@hotmail.it>
2023-05-16 15:30:46 +0200

    DELETE consolelog

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 let firstName = prompt("Qual'é il tuo nome?")
-console.log(firstName)
 let surname = prompt("Qual'é il tuo cognome?")
-console.log(surname)
 let favouriteColor = prompt("Qual'é il tuo colore preferito?")
-console.log(favouriteColor)
 document.write(firstName + surname + favouriteColor + '21');
