a73db44dda0ec209f7838f25695f3f4d666baee4
andreacassini <andrea1316@hotmail.it>
2023-05-16 15:30:07 +0200

    Done esercizio svolto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Qual'é il tuo nome?")
+console.log(firstName)
+let surname = prompt("Qual'é il tuo cognome?")
+console.log(surname)
+let favouriteColor = prompt("Qual'é il tuo colore preferito?")
+console.log(favouriteColor)
+document.write(firstName + surname + favouriteColor + '21');
